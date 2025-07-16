b3ee80a1e6a0eeaf5bd21d441d2756cddae78242
Chrisdev2330 <christianmorenodumit@gmail.com>
2025-07-16 11:27:22 -0400

    lsitoo yaa

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ async def webhook_whatsapp():
     return jsonify({"status": "success"}), 200
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
